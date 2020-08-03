3bf7d869fb281165e0957b2200747c3555c123d3
Inna Smirnova <moipr@yandex.ru>
2020-08-04 02:50:55 +0300

    Modified MobileBankApiTestV4

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -3,7 +3,6 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 class MobileBankApiTestV4 {
     @Test
@@ -18,7 +17,6 @@ class MobileBankApiTestV4 {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
         ;
     }
 }
